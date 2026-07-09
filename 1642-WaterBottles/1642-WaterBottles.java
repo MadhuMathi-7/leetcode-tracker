95df0ca74b4f073b89c91ba11ec7198114e276ea
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:49:25 +0530

    Create 1642-WaterBottles

---
+++ 1642-WaterBottles/1642-WaterBottles.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 9:49:24 AM
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalBottles = numBottles;
+
+        while (numBottles >= numExchange) {
+            totalBottles += numBottles / numExchange;
+            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        }
+
+        return totalBottles;
+    }
+}
