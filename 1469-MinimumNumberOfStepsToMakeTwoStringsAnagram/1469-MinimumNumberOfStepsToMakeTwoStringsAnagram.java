1e6a2c6325cbc0dd526e97202106553bb7a35af7
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:49:30 +0530

    Create 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram

---
+++ 1469-MinimumNumberOfStepsToMakeTwoStringsAnagram/1469-MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 9:49:29 AM
+class Solution {
+    public int minSteps(String s, String t) {
+        int a[]=new int[26];
+        int b[]=new int[26];
+        for(char c:s.toCharArray())
+        {
+            a[c-'a']++;
+        }
+        for(char c:t.toCharArray())
+        {
+            b[c-'a']++;
+        }
+        int ans=0;
+        for(int i=0;i<26;i++)
+        {
+            if(a[i]>b[i])
+            {
+                ans+=a[i]-b[i];
+            }
+        }
+        return ans;
+    }
+}
