682295f1d5441280003912706941efb9dc5471bc
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:52:25 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 9:52:24 AM
+import java.math.BigInteger;
+
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger n1 = new BigInteger(a, 2);
+        BigInteger n2 = new BigInteger(b, 2);
+        BigInteger sum = n1.add(n2);
+        return sum.toString(2); // convert back to binary
+    }
+}
+
