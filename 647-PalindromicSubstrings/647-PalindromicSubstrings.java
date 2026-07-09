6b18bf56f96554668b10fcacbb51ce5bccbfd74c
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:50:12 +0530

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:50:09 AM
+class Solution {
+    public int countSubstrings(String S) {
+        int len = S.length(), ans = 0;
+        for (int i = 0; i < len; i++) {
+            int j = i - 1, k = i;
+            while (k < len - 1 && S.charAt(k) == S.charAt(k+1)) k++;
+            ans += (k - j) * (k - j + 1) / 2;
+            i = k++;
+            while (j >= 0 && k < len && S.charAt(k++) == S.charAt(j--)) ans++;
+        }
+        return ans;
+    }
+}
