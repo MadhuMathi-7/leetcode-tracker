ab11afff0b1324edb0e2f83407be52a399aef5dc
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:49:41 +0530

    Create 1260-DayOfTheYear

---
+++ 1260-DayOfTheYear/1260-DayOfTheYear.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:49:40 AM
+import java.time.LocalDate;
+class Solution{
+    public static int dayOfYear(String date){
+    	int n=date.length();
+    	LocalDate d=LocalDate.parse(date);
+    	return d.getDayOfYear();
+    }
+}
