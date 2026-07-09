7586d6953ec422f3ed611f61c2803487ff8c281d
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:49:19 +0530

    Create 1806-CountOfMatchesInTournament

---
+++ 1806-CountOfMatchesInTournament/1806-CountOfMatchesInTournament.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:49:17 AM
+class Solution {
+    public int numberOfMatches(int n) {
+        int matches = 0;
+        
+        while (n > 1) {
+            if (n % 2 == 0) {  
+                matches += n / 2;
+                n /= 2;
+            } else {  
+                matches += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        
+        return matches;
+    }
+}
