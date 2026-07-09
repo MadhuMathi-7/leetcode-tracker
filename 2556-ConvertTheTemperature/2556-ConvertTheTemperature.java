54a8e8d3534cddba8cefbb5c3be402f7a1c50b5a
Madhu Mathi R <madhumathiravikrishnan@gmail.com>
2026-07-09 09:48:54 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,8 @@
+// Last updated: 7/9/2026, 9:48:51 AM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double fahrenheit = celsius * 1.80 + 32.00;
+        return new double[]{kelvin, fahrenheit};
+    }
+}
